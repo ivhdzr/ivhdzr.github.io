d5eec6c2d0f344f99cb6b2a33fa5ea016a79cdfc
Iván <ivanhdzr1605@gmail.com>
2025-05-07 02:47:52 -0600

    ajustes nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,9 @@ AOS.init();
 
 const btnSection = document.querySelector("#btn_section");
 const btnTop = document.querySelector("#btn_top");
+const btnMenu = document.getElementById("btn_menu");
+const menu = document.querySelector(".flx-menu");
+const iconMenu = document.querySelector("#icon_menu");
 
 btnSection.addEventListener("click", (e) => {
     // Encontrar el botón más cercano al clic, si existe
@@ -44,6 +47,34 @@ window.addEventListener("scroll", function () {
 btnTop.addEventListener("click", () => {
     window.scrollTo({
         top: 0,
+    });
+});
+
+const iconHamburger = `
+<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
+  class="bi bi-list" viewBox="0 0 16 16">
+  <path fill-rule="evenodd"
+    d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
+</svg>`;
+
+const iconClose = `
+<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
+  class="bi bi-x-lg" viewBox="0 0 16 16">
+  <path
+    d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
+</svg>`;
+
+btnMenu.addEventListener("click", () => {
+    const isOpen = menu.style.left === "0px";
+    menu.style.left = isOpen ? "-100%" : "0px";
+    iconMenu.innerHTML = isOpen ? iconHamburger : iconClose;
+});
+
+menu.querySelectorAll("a").forEach(link => {
+    link.addEventListener("click", () => {
+        const isOpen = menu.style.left === "0px";
+        menu.style.left = isOpen ? "-100%" : "0px";
+        iconMenu.innerHTML = isOpen ? iconHamburger : iconClose;
     });
 });
 
